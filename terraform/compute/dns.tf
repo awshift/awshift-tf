b1a2b28210f13ac5b6ba5b6fe70dcd51435e4dfe
sayk <97113955+saykdoteth@users.noreply.github.com>
2024-12-12 15:02:22 +0100

    dns.tf, need to update the records with lb url

---
+++ terraform/compute/dns.tf
@@ -0,0 +1,29 @@
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 5.0"
+
+  domain_name  = "tycm2-infra.fr"
+  zone_id      = "Z2ES7B9AZ6SHAE"
+
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.tycm2-infra.fr",
+    "awshift.tycm2-infra.fr" 
+  ]
+
+  wait_for_validation = true
+
+  tags = {
+    Name = "tycm2-infra.fr"
+  }
+}
+
+resource "aws_route53_record" "awshift" {
+  zone_id = "Z2ES7B9AZ6SHAE"
+  name    = "awshift.tycm2-infra.fr"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = [""]
+}
