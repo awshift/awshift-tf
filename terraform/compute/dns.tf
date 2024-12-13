284112ce6559cd8dae93481f91e90d7ddf0e996a
quaktuss <mathieupheron1@gmail.com>
2024-12-13 15:22:49 +0100

    fix: change records type

--- terraform/compute/dns.tf
+++ terraform/compute/dns.tf
@@ -29,5 +29,5 @@ resource "aws_route53_record" "awshift" {
   type    = "CNAME"
   ttl     = 300
 
-  records = [module.master_instances[0].public_ips[0]] # Load Balancer url to add
+  records = module.master_instances[0].public_ips[0]
 }
