305b2beed79669b82ce98d47024bf85e74c9e86e
mathieu.pheron <mathieu.pheron@soprasteria.com>
2025-02-03 15:57:50 +0100

    fix: modify gitignore and fix outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "Bastion" {
+output "bastion_public_ip" {
   value = module.bastion_instance.public_ips
 }
 
-output "Master Private IP" {
+output "master_private_ip" {
   value = module.master_instances[0].public_ips
 }
 
-output "Workers Private IP" {
+output "workers_private_ips" {
   value = module.worker_instances[*].private_ips
 }
